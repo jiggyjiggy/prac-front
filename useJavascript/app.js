35adef9f8948e88fbd1491040136aacea4764045
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-09-25 21:52:01 +0900

    refactor: Craete template

--- useJavascript/app.js
+++ useJavascript/app.js
@@ -17,8 +17,19 @@ function getData(url) {
 function newsFeed() {
     const newsFeed = getData(NEWS_URL);
     const newList = [];
+    let template = `
+        <div class="container mx-auto p-4">
+            <h1>Hacker News</h1>
+            <ul>
+                {{__news_feed__}}      
+            </ul>
+            <div>
+                <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+                <a href="#/page/{{__next_page__}}">다음 페이지</a>
+            </div>
+        </div>
+    `;
 
-    newList.push(`<ul>`);
     for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newList.push(`
             <li>
@@ -28,16 +39,12 @@ function newsFeed() {
             </li>
         `);
     };
-    newList.push(`</ul>`);
 
-    newList.push(`
-        <div>
-            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
-            <a href="#/page/${store.currentPage < 3 ? store.currentPage + 1 : 3}">다음 페이지</a>
-        </div>
-    `);
+    template = template.replace("{{__news_feed__}}", newList.join(""));
+    template = template.replace("{{__prev_page__}}", store.currentPage - 1 < 1 ? 1 : store.currentPage - 1);
+    template = template.replace("{{__next_page__}}", store.currentPage + 1 > 3 ? 3 : store.currentPage + 1);
 
-    container.innerHTML = newList.join("");
+    container.innerHTML = template;
 };
 
 function newsDetail() {
