65a2278f57bdef307d894a331b7ed92bf85a8a2d
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-09-25 13:05:39 +0900

    feat: HN client

---
+++ 3/app.js
@@ -0,0 +1,15 @@
+let ajax = new XMLHttpRequest();
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json"
+ajax.open("GET", NEWS_URL, false);
+ajax.send();
+
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 10; i++){
+    const li = document.createElement("li");
+    li.innerText = newsFeed[i].title;
+    ul.appendChild(li);
+
+}
+document.getElementById("root").appendChild(ul);
