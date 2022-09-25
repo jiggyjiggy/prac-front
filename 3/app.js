5ba4faafe48c5edc2ddd4e420a4a725a0654bf65
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-09-25 16:26:10 +0900

    feat: Add event listener; hashchange

--- 3/app.js
+++ 3/app.js
@@ -1,15 +1,39 @@
-let ajax = new XMLHttpRequest();
-const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json"
+const ajax = new XMLHttpRequest();
+const content = document.createElement("div");
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+
 ajax.open("GET", NEWS_URL, false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement("ul");
 
-for (let i = 0; i < 10; i++){
-    const li = document.createElement("li");
-    li.innerText = newsFeed[i].title;
-    ul.appendChild(li);
+window.addEventListener("hashchange", function() {
+    const id = location.hash.substring(1);
 
-}
+    ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+    ajax.send();
+
+    const newsContent = JSON.parse(ajax.response);
+    const title = document.createElement("h1");
+    title.innerHTML = newsContent.title;
+
+    content.appendChild(title);
+});
+
+for (let i = 0; i < 10; i++) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+
+    a.href = `#${newsFeed[i].id}`
+    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+    a.addEventListener("click", function() {});   
+
+    li.appendChild(a);
+    ul.appendChild(li);
+};
+
 document.getElementById("root").appendChild(ul);
+document.getElementById("root").appendChild(content);
